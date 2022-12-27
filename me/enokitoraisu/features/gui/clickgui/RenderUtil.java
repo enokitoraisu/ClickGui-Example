493709a62a5265166520098c518a7595c813ef43
enokitoraisu <96944846+enokitoraisu@users.noreply.github.com>
2022-12-28 00:20:48 +0900

    Create RenderUtil.java

---
+++ me/enokitoraisu/features/gui/clickgui/RenderUtil.java
@@ -0,0 +1,22 @@
+public class RenderUtil {
+    public static void rect(float x, float y, float width, float height, int color) {
+        float red = (float) (color >> 16 & 255) / 255.0F;
+        float green = (float) (color >> 8 & 255) / 255.0F;
+        float blue = (float) (color & 255) / 255.0F;
+        float alpha = (float) (color >> 24 & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.color(red, green, blue, alpha);
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION);
+        bufferbuilder.pos(x, y + height, 0.0D).endVertex();
+        bufferbuilder.pos(x + width, y + height, 0.0D).endVertex();
+        bufferbuilder.pos(x + width, y, 0.0D).endVertex();
+        bufferbuilder.pos(x, y, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+}
