b10219a659a033f0fa69f0ac0936664acf7d5e5e
enokitoraisu <96944846+enokitoraisu@users.noreply.github.com>
2023-04-03 12:34:02 +0900

    miss

--- me/enokitoraisu/features/gui/clickgui/CategoryPanel.java
+++ me/enokitoraisu/features/gui/clickgui/CategoryPanel.java
@@ -1,7 +1,5 @@
-package me.enokitoraisu.features.gui.clickgui2;
+package me.enokitoraisu.features.gui.clickgui;
 
-import me.enokitoraisu.features.module.Category;
-import me.enokitoraisu.features.module.Module;
 import net.minecraft.client.Minecraft;
 
 import java.util.ArrayList;
