e3637a5f76b347863142fcf4f3f1b0c41139cc23
enokitoraisu <96944846+enokitoraisu@users.noreply.github.com>
2023-07-13 10:25:54 +0900

    Update ItemColor.java

--- me/enokitoraisu/features/gui/clickgui/item/items/ItemColor.java
+++ me/enokitoraisu/features/gui/clickgui/item/items/ItemColor.java
@@ -14,7 +14,7 @@ public class ItemColor extends Item<ColorSetting> {
     private float settingHue, settingSaturation, settingBrightness;
     private boolean movingPicker;
     private boolean isSlideHue;
-  
+
     public ItemColor(ColorSetting colorSetting, int x, int y, int width, int height) {
         super(colorSetting, x, y, width, height);
         float[] hsb = Color.RGBtoHSB(getObject().getValue().getRed(), getObject().getValue().getGreen(), getObject().getValue().getBlue(), new float[3]);
@@ -28,37 +28,37 @@ public class ItemColor extends Item<ColorSetting> {
         if (!getObject().isVisible()) return 0;
         this.offset = offset;
         int y = this.y + offset;
-    
-        RenderUtil.rect(x + 2, y + 2, height - 4, height - 4, getIntValue());
+
+        RenderUtil.rect(x + 2, y + 2, height - 4, height - 4, getObject().getValue().hashCode());
         mc.fontRenderer.drawStringWithShadow(getObject().getName(), x + height + 5, y + height / 2F - mc.fontRenderer.FONT_HEIGHT / 2F, -1);
         drawColorPicker(x + 3, y + height + 3, width - 6, width - 18, Color.HSBtoRGB(settingHue, settingSaturation, settingBrightness));
-    
+
         //current saturation brightness
-        RenderUtil.rect(x + 3 + (width - 6) * settingSaturation, y + 3 + (width - 18) * settingBrightness, 1, 1, 0x80000000);
+        RenderUtil.rect(x + 3 + (width - 6) * settingSaturation, y + height + 3 + (width - 18) * (1 - settingBrightness), 1, 1, 0x80000000);
         for (int hueX = 0; hueX < width - 6; hueX++) {
-            RenderUtil.rect(x + 3 + hueX, y + 3 + width - 15, 1, 12, Color.HSBtoRGB((float) hueX / (width - 6), 1F, 1F));
+            RenderUtil.rect(x + 3 + hueX, y + height + 3 + width - 15, 1, 12, Color.HSBtoRGB((float) hueX / (width - 6), 1F, 1F));
         }
         //current hue
-        RenderUtil.rect(x + 3 + (width - 6) * settingHue, y + 3 + width - 15, 1, 12, 0x80000000);
-    
+        RenderUtil.rect(x + 3 + (width - 6) * settingHue, y + height + 3 + width - 15, 1, 12, 0x80000000);
+
         if (movingPicker) {
             settingSaturation = MathHelper.clamp(((float) mouseX - (x + 3)) / (width - 6), 0F, 1F);
-            settingBrightness = MathHelper.clamp(((float) mouseY - (y + height + 3)) / (width - 18), 0F, 1F);
+            settingBrightness = 1 - MathHelper.clamp(((float) mouseY - (y + height + 3)) / (width - 18), 0F, 1F);
             getObject().setValue(Color.getHSBColor(settingHue, settingSaturation, settingBrightness));
         } else if (isSlideHue) {
             settingHue = MathHelper.clamp(((float) mouseX - (x + 3)) / (width - 6), 0F, 1F);
             getObject().setValue(Color.getHSBColor(settingHue, settingSaturation, settingBrightness));
         }
-        
+
         return width + height;
     }
-  
+
     @Override
     public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
         if (!getObject().isVisible()) return;
         if (bounding(mouseX, mouseY, x + 3, y + height + 3, width - 6, width - 18))
             movingPicker = true;
-        else if (bounding(mouseX, mouseY, x + 3, y + 3 + width - 15, width - 6, 12))
+        else if (bounding(mouseX, mouseY, x + 3, y + height + 3 + width - 15, width - 6, 12))
             isSlideHue = true;
     }
 
@@ -67,13 +67,13 @@ public class ItemColor extends Item<ColorSetting> {
         movingPicker = false;
         isSlideHue = false;
     }
-  
+
     public void drawColorPickerShade(int x, int y, int width, int height, int color) {
         color = fixHue(color);
         float red = (float) (color >> 16 & 0xFF) / 255.0f;
         float green = (float) (color >> 8 & 0xFF) / 255.0f;
         float blue = (float) (color & 0xFF) / 255.0f;
-    
+
         GlStateManager.disableTexture2D();
         GlStateManager.enableBlend();
         GlStateManager.disableAlpha();
@@ -92,7 +92,7 @@ public class ItemColor extends Item<ColorSetting> {
         GlStateManager.enableAlpha();
         GlStateManager.enableTexture2D();
     }
-  
+
     //lagggg
     public void drawColorPicker(int x, int y, int width, int height, int color) {
         int red = color >> 16 & 0xFF;
@@ -100,14 +100,14 @@ public class ItemColor extends Item<ColorSetting> {
         int blue = color & 0xFF;
         float hue = Color.RGBtoHSB(red, green, blue, new float[3])[0];
         for (int colorX = 0; colorX < width; colorX++) {
-            for (int colorY = 0; colorY < height; colorX++) {
+            for (int colorY = 0; colorY < height; colorY++) {
                 float saturation = (float) colorX / width;
                 float brightness = 1F - (float) colorY / height;
                 RenderUtil.rect(x + colorX, y + colorY, 1, 1, Color.HSBtoRGB(hue, saturation, brightness));
             }
         }
     }
-  
+
     public int fixHue(int color) {
         int red = color >> 16 & 0xFF;
         int green = color >> 8 & 0xFF;
